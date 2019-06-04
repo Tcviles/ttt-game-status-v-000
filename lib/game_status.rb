da2fdc1c7ddafac3fbdfed659a29aa451c0d3748
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-04 00:26:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,9 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    ((board[combo[0]] == board[combo[1]]) and (board[combo[1]] == board[combo[2]]) and (position_taken?(board,combo[0]))) ? combo : nil
-  end
+  WIN_COMBINATIONS.detect {|combo| ((board[combo[0]] == board[combo[1]]) and (board[combo[1]] == board[combo[2]]) and (position_taken?(board,combo[0]))) ? combo : nil }
 end
 
 def full?(board)
